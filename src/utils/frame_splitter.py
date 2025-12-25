d91fa45c01ddcff91da2f8260f4143ae81410706
kacperfilip <kacperf.dev@gmail.com>
2025-12-25 10:03:23 +0100

    feat: Implement frame_splitter logic

---
+++ src/utils/frame_splitter.py
@@ -0,0 +1,137 @@
+import os
+from typing import Tuple, Dict
+import cv2
+
+
+def split_frames(raw_videos_path: str, output_path: str, truncate_output_dir: bool, target_total_images: int = 200,
+                 target_porch_with_cat_percentage: float = 0.7, target_empty_porch_percentage: float = 0.15,
+                 target_other_percentage: float = 0.15) -> None:
+    """
+    Extracts frames from raw videos and saves them in a specified output directory
+    :param raw_videos_path -- path to the directory containing raw videos
+    :param output_path -- path to the output directory
+    :param truncate_output_dir -- whether to truncate the output directory before writing new files
+    :param target_total_images -- total number of frames to extract from all videos
+    :param target_porch_with_cat_percentage -- percentage of frames to extract from porch videos with a cat
+    :param target_empty_porch_percentage -- percentage of frames to extract from empty porch videos
+    :param target_other_percentage -- percentage of frames to extract from indoor videos
+    :return -- None
+    """
+    total_saved_frames = 0
+    total_processed_frames = 0
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        print("Created output directory")
+
+    if truncate_output_dir:
+        for file in os.listdir(output_path):
+            os.remove(os.path.join(output_path, file))
+
+    frame_count_dict = count_total_frames(raw_videos_path)
+
+    frames_to_extract_porch_cat = int(target_total_images * target_porch_with_cat_percentage)
+    frames_to_extract_empty_porch = int(target_total_images * target_empty_porch_percentage)
+    frames_to_extract_other = int(target_total_images * target_other_percentage)
+
+    frame_info_porch_cat = extract_frames(raw_videos_path, output_path, "porch_cat", frame_count_dict, frames_to_extract_porch_cat)
+    frame_info_empty_porch = extract_frames(raw_videos_path, output_path, "empty_porch", frame_count_dict, frames_to_extract_empty_porch)
+    frame_info_other = extract_frames(raw_videos_path, output_path, "other", frame_count_dict, frames_to_extract_other)
+
+    total_processed_frames += frame_info_porch_cat[0] + frame_info_empty_porch[0] + frame_info_other[0]
+    total_saved_frames += frame_info_porch_cat[1] + frame_info_empty_porch[1] + frame_info_other[1]
+    total_video_count = frame_count_dict["porch_cat"]["number_of_videos"] + frame_count_dict["empty_porch"]["number_of_videos"] + frame_count_dict["other"]["number_of_videos"]
+
+    print(f"\nTotal number of processed frames: {total_processed_frames}")
+    print(f"Total number of saved frames: {total_saved_frames}")
+    print(f"Total number of videos processed: {total_video_count}")
+
+
+def extract_frames(video_path: str, output_path: str, video_type: str, frame_count_dict: dict, frames_to_extract) -> Tuple[int, int]:
+    """
+    Extracts frames from a video and saves them in a specified output directory
+    :param video_path -- path to the directory containing raw videos
+    :param output_path -- path to the output directory
+    :param video_type -- type of video to extract frames from (porch_cat, empty_porch, other)
+    :param frame_count_dict -- dictionary containing information about the total number of frames in each video type
+    :param frames_to_extract -- number of frames to extract from each video
+    :return -- a tuple containing the total number of processed frames and the total number of saved frames
+    """
+    total_saved_frames = 0
+    total_processed_frames = 0
+    iterator_step = frame_count_dict[video_type]["total_frames"] // frames_to_extract
+    files = [ os.path.join(video_path, path) for path in frame_count_dict[video_type]["videos"].keys() ]
+
+    for file in files:
+        print(f"-- Processing {file} --")
+        cap = cv2.VideoCapture(file)
+        ret, image = cap.read()
+        count = 0
+        while ret:
+            total_processed_frames += 1
+            if total_processed_frames % iterator_step == 0:
+                print(f"Saving frame {count} from {file}")
+                cv2.imwrite(os.path.join(output_path, f"{video_type}_{total_saved_frames}_{count}.jpg"), image)
+                total_saved_frames += 1
+            ret, image = cap.read()
+            count += 1
+
+    return total_processed_frames, total_saved_frames
+
+
+def count_total_frames(video_path: str) -> Dict[str, Dict[str, int | Dict[str, int]]]:
+    """
+    Counts the total number of frames in all input videos
+    :param video_path -- path to the directory containing raw videos
+    :return -- a tuple ([total_porch_with_cat_frames], [total_empty_porch_frames], [total_other_frames])])
+    """
+    frame_count_dict = {
+        "porch_cat": {
+            "total_frames": 0,
+            "number_of_videos": 0,
+            "videos": {}
+        },
+        "empty_porch": {
+            "total_frames": 0,
+            "number_of_videos": 0,
+            "videos": {}
+        },
+        "other": {
+            "total_frames": 0,
+            "number_of_videos": 0,
+            "videos": {}
+        }
+    }
+
+    porch_with_cat_files = [ file_name for file_name in os.listdir(video_path)
+                             if file_name.startswith("porch_cat") and file_name.endswith(".mp4") ]
+    empty_porch_files = [ file_name for file_name in os.listdir(video_path)
+                             if file_name.startswith("empty_porch") and file_name.endswith(".mp4") ]
+    other_files = [ file_name for file_name in os.listdir(video_path)
+                             if file_name.startswith("other") and file_name.endswith(".mp4") ]
+
+    for file in porch_with_cat_files:
+        cap = cv2.VideoCapture(os.path.join(video_path, file))
+        video_frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_count_dict["porch_cat"]["videos"][file] = video_frame_count
+        frame_count_dict["porch_cat"]["total_frames"] += video_frame_count
+        frame_count_dict["porch_cat"]["number_of_videos"] += 1
+        cap.release()
+
+    for file in empty_porch_files:
+        cap = cv2.VideoCapture(os.path.join(video_path, file))
+        video_frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_count_dict["empty_porch"]["videos"][file] = video_frame_count
+        frame_count_dict["empty_porch"]["total_frames"] += video_frame_count
+        frame_count_dict["empty_porch"]["number_of_videos"] += 1
+        cap.release()
+
+    for file in other_files:
+        cap = cv2.VideoCapture(os.path.join(video_path, file))
+        video_frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_count_dict["other"]["videos"][file] = video_frame_count
+        frame_count_dict["other"]["total_frames"] += video_frame_count
+        frame_count_dict["other"]["number_of_videos"] += 1
+        cap.release()
+
+    return frame_count_dict
